9a9284098c658ea62ae5af2bf43237000f34d722
alsrl8259 <alsrl8259@gmail.com>
2021-04-09 16:20:24 +0900

    swe

---
+++ bg.js
@@ -0,0 +1,20 @@
+const body = document.querySelector("body");
+
+const IMG_NUMBER = 4;
+
+function paintImage(imgNumber){
+    const image = new Image();
+    image.src = `images/${imgNumber+1}.jpg`;
+    image.classList.add("bgImage");
+    body.prepend(image);
+}
+function genRandom(){
+    const number = Math.floor(Math.random() * IMG_NUMBER);
+    return number;
+}
+function init(){
+    const reandomNumber = genRandom();
+    paintImage(reandomNumber);
+}
+
+init();
